8cc9b02c0858ee74806b0267df212aa05bae271d
Atul <atul@ktds.co>
2019-02-25 18:28:01 +0530

    add redux to the project

--- App.js
+++ App.js
@@ -8,11 +8,16 @@
 
 import React, {Component} from 'react';
 import Router from 'src/Router';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import reducers from 'src/core/redux/reducers';
 
 export default class App extends Component<Props> {
   render() {
     return (
-      <Router />
+      <Provider store={createStore(reducers)}>
+        <Router />
+      </Provider>
     );
   }
 }
